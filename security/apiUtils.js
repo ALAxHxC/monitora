dc3777f71f4c5eb27fa9bf8ba688dfad3b4735ae
ALAxHxC <jenizaro123>
2018-07-01 12:53:34 -0500

    Proceso de encriptacion

--- security/apiUtils.js
+++ security/apiUtils.js
@@ -4,7 +4,7 @@ const password_external=process.env.SECRET || "e4b749e81288302501f897996a364797"
 const iv_external=process.env.IVI || "ed82a7c7b7a202dc";
 const iv_internal=process.env.IVE ||  "547375022f58e14e";
 const password_internal=process.env.SECRET || "54487333233e1b2b0f76f437d70b60cc";
-
+const aes256 = require('nodejs-aes256');
 ///Encriptation In
 module.exports.encryptInternal = async function(text)
 {
@@ -20,37 +20,44 @@ module.exports.decryptInternal = async function(text)
 
 
 
-///Encriptation out
+///Encriptation out	
 module.exports.encryptExternal = async function(text)
 {
-	let encrypt = await crypto2.encrypt(text,password_external,iv_external);
+	/*let encrypt = await crypto2.encrypt(text,password_external,iv_external);
+	return encrypt;*/
+	let encrypt = aes256.encrypt(text,password_external);
 	return encrypt;
 }
 
 module.exports.decryptExternal = async function(text)
 {
-	let decrypt = await crypto2.decrypt(text,password_external,iv_external);
-	return decrypt;
+	/*let decrypt = await crypto2.decrypt(text,password_external,iv_external);
+	return decrypt;*/
+	let encrypt = aes256.decrypt(text,password_external);
+	return encrypt;
 }
 
 async function test()
 {
+	try{
 	// Create a password from a secret key.
 	 // const password = await crypto2.createPassword('secret');
-
 	  // Create a one-time valid initialization vector.
 	  //const iv =await crypto2.createIv('secret');
-
 	  // Encrypt your plain text, in this case 'the native web'.
-	 // const encrypted = await crypto2.encrypt("elianadreanniñopedrazza",password_external,iv_external);
+	 const encrypted = aes256.encrypt(password_external,"daniel");
+	 const descrypt= aes256.decrypt(password_external,encrypted)
 	  //await crypto2.encrypt('the native web', password_internal, iv_internal);
-	const descryo = await crypto2.decrypt("ae14c248d995d325bbfe64daa16dd85a",password_external,iv_external);
-	const descryo2 = await crypto2.decrypt("9999b778bfaf1d24c7c5a444c647fddb",password_external,iv_external);
-
+	//const descryo = await crypto2.decrypt("ae14c248d995d325bbfe64daa16dd85a",password_external,iv_external);
+	//const descryo2 = await crypto2.decrypt("9999b778bfaf1d24c7c5a444c647fddb",password_external,iv_external);
 	//await crypto2.decrypt(encrypted, password_internal, iv_internal);
 	 // console.log(`Password: ${password}`);
 	//  console.log(`Iv: ${iv}`);
-	 	console.log(`Encrypted: ${descryo2}`);
-	   	console.log(`Encrypted: ${descryo}`);	
+	 	console.log(`Encrypted: ${encrypted}`);
+	 	console.log(`dEncrypted: ${descrypt}`);
+	  // 	console.log(`Encrypted: ${descryo}`);	
+	}catch(err){
+		console.log(err.stack,err.cause)
+	}
 }
 test();
